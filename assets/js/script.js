49efc6c6a0e6e5a9c5d81e0cbb96f91f72942295
CharlieApell <charlie.apell@gmail.com>
2023-03-21 09:27:30 +0000

    added a how to pop-up-window

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ const moles = document.querySelectorAll('.mole');
 const countdownBoard = document.querySelector('.countdown');
 const startButton = document.querySelector('.startButton');
 const sound = new Audio("assets/media/smash.mp3");
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
 let lastHole;
 let timeUp = false;
@@ -74,3 +77,36 @@ window.addEventListener('mousemove', e => {
 });
 
 moles.forEach (mole => mole.addEventListener('click', whack));
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+  
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+  
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+    })
+  })
+  
+  function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
